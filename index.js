7faa50b9e1d047aad6504b5ef9c05e33ac6fda99
Yash <yash@mahitechllc.com>
2023-01-14 14:16:49 +0530

    image limit issue fixed

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 
 app.use(cookieParser());
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: "40mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 dotenv.config({
   path: "./config.env",
