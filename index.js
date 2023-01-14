78f954542df32d3ad34d71d70b692f977ea4faf7
Yash <31120678+yash-js@users.noreply.github.com>
2023-01-14 14:11:40 +0530

    Limit fix

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(express.limit(1000000000000));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 dotenv.config({
   path: "./config.env",
