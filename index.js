5aa50152bf466e70dd55b3b6161216c197b693c1
Yash <yash@mahitechllc.com>
2023-01-17 23:58:40 +0530

    Cors issue fixed

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+);
 app.use(express.json({ limit: "40mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
