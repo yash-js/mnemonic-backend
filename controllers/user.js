83c6c4fe7fdfc6c3e474b44d748508a19dc2ca90
Yash <yash@mahitechllc.com>
2023-01-20 18:10:53 +0530

    User Edit Notification schema added

--- controllers/user.js
+++ controllers/user.js
@@ -38,13 +38,9 @@ exports.searchUser = async (req, res) => {
   try {
     const { query } = req.params;
 
-    const result = await User.find(
-      {
-          "$or":[
-              {firstName:{$regex:query}},
-              {username:{$regex:query}}
-          ],
-          _id: {
+    const result = await User.find({
+      $or: [{ firstName: { $regex: query } }, { username: { $regex: query } }],
+      _id: {
         $nin: req.user._id,
       },
     });
@@ -63,6 +59,66 @@ exports.searchUser = async (req, res) => {
     console.log(error);
     return res.status(400).json({
       error: "Something Went Wrong!",
+    });
+  }
+};
+
+exports.editUser = async (req, res) => {
+  try {
+    let editData = {};
+    const id = req.user._id;
+
+    const { firstName, lastName, email, username } = req.body;
+    // if (!data)
+    //   return res.status(400).json({
+    //     error: "Something Went Wrong! Please Try Again",
+    //   });
+
+    if (
+      req.user.firstName === firstName &&
+      req.user.lastName === lastName &&
+      req.user.email === email &&
+      req.user.username === username
+    ) {
+      return res.status(400).json({
+        error: "Nothing to Update!",
+      });
+    } else if (req.user.firstName !== firstName) {
+      editData.firstName = firstName;
+    } else if (req.user.lastName !== lastName) {
+      editData.lastName = lastName;
+    } else if (req.user.email !== email) {
+      editData.email = email;
+    } else if (req.user.username !== username) {
+      editData.username = username;
+    } else if (req.user.profilePic !== profilePic) {
+      editData.profilePic = profilePic;
+    }
+     if (editData && editData?.username) {
+      const existingUsername = await User.findOne({
+        username: editData.username,
+      });
+      if (existingUsername)
+        return res.status(400).json({
+          error: "Username already taken, Please choose different username!",
+        });
+    }
+    if (editData && editData?.email) {
+      const existingEmail = await User.findOne({ email: editData?.email });
+      if (existingEmail)
+        return res.status(400).json({
+          error: "Username already taken, Please choose different username!",
+        });
+    }
+    await User.findByIdAndUpdate(id, editData);
+
+    return res.json({
+      message: "Profile Updated!",
+    });
+  } catch (error) {
+    res.json({
+      error: "Something Went Wrong!",
+      errMsg: error.message,
     });
   }
 };
