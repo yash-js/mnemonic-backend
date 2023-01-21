be75be7dad4ceae6a4537878d4825fc428402a73
Yash <31120678+yash-js@users.noreply.github.com>
2023-01-21 12:31:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const options = {
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
 app.use(cors(options));
