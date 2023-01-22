8a6086a8a91f23dadbc56b6fa8fcafb0b120b1a6
Yash <yashpurani8@gmail.com>
2023-01-23 00:11:47 +0530

    notes crud api added

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { createNote } = require("../controllers/notes");
+const {
+  createNote,
+  editNote,
+  deleteNote,
+  getNotes,
+} = require("../controllers/notes");
 const authenticate = require("../middlewares/auth");
 const router = express.Router();
 
 router.post("/create", authenticate, createNote);
+router.put("/edit/:id", authenticate, editNote);
+router.delete("/delete/:id", authenticate, deleteNote);
+router.get("/", authenticate, getNotes);
 
 module.exports = router;
